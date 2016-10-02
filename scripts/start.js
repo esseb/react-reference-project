dda8f2336f8178b2c2a1b4d3d2c000d6aa6aa821
Stein Cato Blostrupmoen <esseb@esseb.com>
2016-10-02 22:41:21 +0200

    Remove unused onProxyError function from start.js

--- scripts/start.js
+++ scripts/start.js
@@ -101,34 +101,6 @@ function setupCompiler (host, port, protocol) {
   })
 }
 
-// We need to provide a custom onError function for httpProxyMiddleware.
-// It allows us to log custom error messages on the console.
-function onProxyError (proxy) {
-  return function (err, req, res) {
-    var host = req.headers && req.headers.host
-    console.log(
-      chalk.red('Proxy error:') +
-      ' Could not proxy request ' + chalk.cyan(req.url) +
-      ' from ' + chalk.cyan(host) + ' to ' + chalk.cyan(proxy) + '.'
-    )
-    console.log(
-      'See https://nodejs.org/api/errors.html#errors_common_system_errors' +
-      ' for more information (' +
-      chalk.cyan(err.code) + ').'
-    )
-    console.log()
-
-    // And immediately send the proper error response to the client.
-    // Otherwise, the request will eventually timeout with ERR_EMPTY_RESPONSE on the client side.
-    if (res.writeHead && !res.headersSent) {
-      res.writeHead(500)
-    }
-    res.end('Proxy error: Could not proxy request ' + req.url + ' from ' +
-      host + ' to ' + proxy + ' (' + err.code + ').'
-    )
-  }
-}
-
 function runDevServer (host, port, protocol) {
   // Hook in the webpack middlewares if we are in development mode.
   app.use(webpackDevMiddleware(compiler, {
