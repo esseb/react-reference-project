8353b4b68c562584494062ea654f1d47914123c3
Stein Cato Blostrupmoen <esseb@esseb.com>
2016-10-02 22:57:36 +0200

    Remove support for .jsx file ending

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -72,10 +72,7 @@ module.exports = {
     // https://github.com/facebookincubator/create-react-app/issues/253
     fallback: paths.nodePaths,
     // These are the reasonable defaults supported by the Node ecosystem.
-    // We also include JSX as a common component filename extension to support
-    // some tools, although we do not recommend using it, see:
-    // https://github.com/facebookincubator/create-react-app/issues/290
-    extensions: ['.js', '.json', '.jsx', ''],
+    extensions: ['.js', '.json', ''],
     alias: {
       // Support React Native Web
       // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
